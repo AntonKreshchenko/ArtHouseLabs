666c38efd2c3730765481419bda681c92306c067
Anton <antonsgrouva@gmail.com>
2018-12-10 23:59:28 +0200

    scroll anim

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -4,3 +4,12 @@ $('.menu-btn').click(function(e) {
     $(this).toggleClass('menu-btn_active');
     $('.header-nav').toggleClass('header-nav_active');
 });
+
+//anchors scroll
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
